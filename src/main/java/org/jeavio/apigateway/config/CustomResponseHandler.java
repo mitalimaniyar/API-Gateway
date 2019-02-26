0a4d6590e396fe21e0f5b3df33269597e6c11c08
mitalimaniyar <mitali@jeavio.com>
2019-02-26 11:14:00 +0530

    Response body processing

--- src/main/java/org/jeavio/apigateway/config/CustomResponseHandler.java
+++ src/main/java/org/jeavio/apigateway/config/CustomResponseHandler.java
@@ -9,6 +9,10 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.util.EntityUtils;
+import org.jeavio.apigateway.model.IntegrationResponse;
+import org.jeavio.apigateway.model.Swagger;
+import org.jeavio.apigateway.service.IntegrationService;
+import org.jeavio.apigateway.service.URLMethodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,24 +22,47 @@ public class CustomResponseHandler {
 
 	@Autowired
 	HttpServletRequest request;
-	
+
+	@Autowired
+	IntegrationService integrationService;
+
+	@Autowired
+	URLMethodService urlMethodService;
+
+	@Autowired
+	Swagger swagger;
+
 	@Bean
-	public ResponseHandler<String> getResponseHandler(){
+	public ResponseHandler<String> getResponseHandler() {
 		ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
 
-            @Override
-            public String handleResponse(
-                    final HttpResponse myresponse) throws ClientProtocolException, IOException {
-                int status = myresponse.getStatusLine().getStatusCode();
-                if (status >= 200 && status < 300) {
-                    HttpEntity entity = myresponse.getEntity();
-                    
-                    return entity != null ? EntityUtils.toString(entity) : null;
-                } else {
-                    throw new ClientProtocolException("Unexpected response status: " + status);
-                }
-            }
-        };
-        return responseHandler;
+			@Override
+			public String handleResponse(final HttpResponse myresponse) throws ClientProtocolException, IOException {
+				Integer status = myresponse.getStatusLine().getStatusCode();
+
+				String uri = request.getRequestURI();
+				String method = request.getMethod();
+
+				IntegrationResponse response;
+				response = integrationService.getIntegrationObject(uri, method).getResponses().get(status.toString());
+				if (response == null) {
+					response=integrationService.getIntegrationObject(uri, method).getResponses().get("default");
+					String expectedstatus = response.getStatusCode();
+					
+					if(!expectedstatus.equals(status.toString()))
+						throw new ClientProtocolException("Unexpected response status: " + status);
+					
+					
+				}
+				if (status >= 200 && status < 300) {
+					HttpEntity entity = myresponse.getEntity();
+
+					return entity != null ? EntityUtils.toString(entity) : null;
+				} else {
+					throw new ClientProtocolException("Unexpected response status: " + status);
+				}
+			}
+		};
+		return responseHandler;
 	}
 }
